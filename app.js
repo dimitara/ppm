103f420451786d3f8cc7d951cb5a6cf71a3bb12d
krassykirov <krassy.kirov@gmail.com>
2015-08-18 15:22:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,54 @@
-var express = require('express');
-var app = express();
+var express=require("express");
+var multer  = require('multer');
+var app=express();
+var done=false;
+var fs = require('fs');
+var path = require('path');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+/*Configure the multer.*/
+
+app.use(multer({ dest: './uploads/',
+    rename: function (fieldname, filename) {
+        return filename+ '-' + Date.now();
+    },
+    onFileUploadStart: function (file) {
+        console.log(file.originalname + ' is starting ...')
+    },
+    onFileUploadComplete: function (file) {
+        console.log(file.fieldname + ' uploaded to  ' + file.path);
+        var destination = './files';
+        try {
+            var stats = fs.statSync(destination);
+        } catch (e) {
+            fs.mkdirSync(destination);
+        }
+        var matches = file.name.match(/-(\d+)(\.[a-z]+){0,1}/);
+        if (!matches) {
+            throw  new Error('Invalid file name!');
+        }
+        var destDirName = matches[1];
+        fs.mkdirSync(path.join(destination, destDirName));
+
+        var destinationPath = path.join(destination, destDirName, matches[1] + (!matches[2] ? '' : matches[2]));
+        fs.writeFileSync(destinationPath, fs.readFileSync(file.path));
+        fs.unlinkSync(file.path);
+        done=true;
+    }
+}));
+
+/*Handling routes.*/
+
+app.get('/',function(req,res){
+    res.sendfile("index.html");
 });
 
-var port = process.env.PORT || 8080;
+app.post('/api/photo',function(req,res){
+    if(done==true){
+        res.end("File uploaded.");
+    }
+});
 
-var server = app.listen(port, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+/*Run the server.*/
+app.listen(3000,function(){
+    console.log("Working on port 3000");
 });
